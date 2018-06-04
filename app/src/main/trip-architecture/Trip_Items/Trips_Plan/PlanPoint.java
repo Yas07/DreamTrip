12256cbaece805824763d6b56c671354333d1c2b
Yas <yaslan859@gmail.com>
2018-06-04 10:07:38 +0300

    First step of integration.

---
+++ app/src/main/trip-architecture/Trip_Items/Trips_Plan/PlanPoint.java
@@ -0,0 +1,46 @@
+package Trip_Items.Trips_Plan;
+
+import java.util.Calendar;
+
+import javax.swing.text.html.ImageView;
+
+import Trip_Items.Places.Place;
+
+public class PlanPoint implements Comparable {
+
+    private String          _title;
+    private Color           _colorBlock;
+    private ImageView       _icon;
+    private Place           _place;
+    private Calendar        _calendar;
+    private String          _otherDetails;
+
+    public PlanPoint(String _title, Color _colorBlock,
+                     ImageView _icon, Place _place,
+                     Calendar _calendar, String _otherDetails) {
+        this._title = _title;
+        this._colorBlock = _colorBlock;
+        this._icon = _icon;
+        this._place = _place;
+        this._calendar = _calendar;
+        if (this._calendar == null) {
+           this._calendar = Calendar.getInstance();
+        }
+        this._otherDetails = _otherDetails;
+    }
+
+
+    @Override
+    public int compareTo(Object o) {
+        PlanPoint p = (PlanPoint)o;
+        return _calendar.compareTo(p._calendar);
+    }
+
+    public Calendar getCalendar() {
+        return _calendar;
+    }
+
+    public void setTime(int y, int m, int d, int hour, int min) {
+        _calendar.set(y, m, d, hour, min,0);
+    }
+}
