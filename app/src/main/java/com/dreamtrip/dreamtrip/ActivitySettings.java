1ef2b247a7215677d356988f1a3dd157e546ae6d
Yas <yaslan859@gmail.com>
2018-06-23 18:53:37 +0300

    settings - button to delete all data

--- app/src/main/java/com/dreamtrip/dreamtrip/ActivitySettings.java
+++ app/src/main/java/com/dreamtrip/dreamtrip/ActivitySettings.java
@@ -1,13 +1,21 @@
 package com.dreamtrip.dreamtrip;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class ActivitySettings extends AppCompatActivity {
-ImageButton btnColor;
+    ImageButton btnDelAll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +25,26 @@ ImageButton btnColor;
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        btnColor = (ImageButton) findViewById(R.id.btnColor);
-        btnColor.setOnClickListener(new View.OnClickListener(){
+        btnDelAll = (ImageButton) findViewById(R.id.btnDelAll);
+        btnDelAll.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                
+                openDialog(v);
             }
         });
     }
+
+    public void openDialog(View view){
+        new AlertDialog.Builder(this)
+                .setTitle("Delete all data")
+                .setMessage("Do you want to delete all your data in app?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        //TODO: delete all data in app
+                    }
+                })
+                .setNegativeButton(android.R.string.no, null).show();
+    }
+
 }
